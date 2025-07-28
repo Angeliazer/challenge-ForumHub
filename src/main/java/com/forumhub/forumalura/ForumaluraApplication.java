c75f16430dd07f1d2a273d040de71264a0b16120
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2025-07-28 12:25:30 -0300

    Primeiro commit

---
+++ src/main/java/com/forumhub/forumalura/ForumaluraApplication.java
@@ -0,0 +1,13 @@
+package com.forumhub.forumalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumaluraApplication.class, args);
+	}
+
+}
